7e0c8c0fbeb13a0abd551a226521bdde74d2aaf7
SeongBinYoon <55236612+SeongBinYoon@users.noreply.github.com>
2020-06-08 20:03:51 +0900

    added ViewActivity.java and WriteActivity.java

---
+++ app/src/main/java/com/example/kimmessicorp/ViewActivity.java
@@ -0,0 +1,14 @@
+package com.example.board4;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
