bb10c75ec1d3adb760c6e0e82c18353632eff70d
yewonkang00 <yewon00000@naver.com>
2020-06-20 14:51:07 +0900

    WriteActivity - Addbutton 클릭 시 수행

--- app/src/main/java/com/example/kimmessicorp/WriteActivity.java
+++ app/src/main/java/com/example/kimmessicorp/WriteActivity.java
@@ -1,10 +1,18 @@
 package com.example.kimmessicorp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class WriteActivity extends AppCompatActivity {
 
@@ -20,6 +28,40 @@ public class WriteActivity extends AppCompatActivity {
         title = findViewById(R.id.title);
         content = findViewById(R.id.content);
         addbutton = findViewById(R.id.add_button);
-        
+
+        //add버튼 클릭 시 수행
+        addbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //EditText에 현재 입력되어 있는 값을 get해옴.
+                String newtitle = title.getText().toString();
+                String newcontent = content.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if (success) {
+                                // 작성 성공
+                                Toast.makeText(getApplicationContext(), "작성되었습니다!", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(WriteActivity.this, MainActivity.class);
+                                //나중에 loginActivity 를 게시판 메인화면으로 바꾸기
+                                startActivity(intent);
+                            } else {
+                                // 작성 실패
+                                Toast.makeText(getApplicationContext(), "작성이 실패되었습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+            }
+        });
     }
 }
