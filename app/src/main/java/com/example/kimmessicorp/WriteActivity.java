f027a0f7fe34c42cff71338d349fd9db8f4a11cf
yewonkang00 <yewon00000@naver.com>
2020-06-20 14:48:48 +0900

    WriteActivity - EditText와 Button의 키 값 배정

--- app/src/main/java/com/example/kimmessicorp/WriteActivity.java
+++ app/src/main/java/com/example/kimmessicorp/WriteActivity.java
@@ -1,14 +1,25 @@
 package com.example.kimmessicorp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WriteActivity extends AppCompatActivity {
 
+    private EditText title, content;
+    private Button addbutton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        //버튼과 EditText의 키 값 배정
+        title = findViewById(R.id.title);
+        content = findViewById(R.id.content);
+        addbutton = findViewById(R.id.add_button);
+        
     }
 }
