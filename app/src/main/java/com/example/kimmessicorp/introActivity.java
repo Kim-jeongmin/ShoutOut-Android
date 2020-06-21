fb955833c87f77dffd1f41808bc81fa1f890f098
yewonkang00 <yewon00000@naver.com>
2020-06-21 22:10:27 +0900

    introActivity - 생성 및 구성

--- app/src/main/java/com/example/kimmessicorp/introActivity.java
+++ app/src/main/java/com/example/kimmessicorp/introActivity.java
@@ -1,8 +1,11 @@
 package com.example.kimmessicorp;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class introActivity extends AppCompatActivity {
 
@@ -11,4 +14,21 @@ public class introActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        //액션 바 숨기기
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        //2초 후 인트로 제거
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(introActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+                finish();
+            }
+        }, 2000);
+
+    }
 }
