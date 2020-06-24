abc652938b917a3b7c7397e1f4f815fbcd412a7f
Kim-jeongmin <55871387+Kim-jeongmin@users.noreply.github.com>
2020-06-24 22:53:57 +0900

    중복 확인 기능 구현

--- app/src/main/java/com/example/kimmessicorp/RegisterActivity.java
+++ app/src/main/java/com/example/kimmessicorp/RegisterActivity.java
@@ -20,7 +20,7 @@ import org.json.JSONObject;
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText et_id,et_pass ,et_name ,et_age;
-    private Button btn_register;
+    private Button btn_register, btn_overlap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +36,7 @@ public class RegisterActivity extends AppCompatActivity {
         et_name = findViewById(R.id.et_name);
         et_age = findViewById(R.id.et_age);
         btn_register = findViewById(R.id.btn_register);
+        btn_overlap = findViewById(R.id.btn_overlap);
 
         // 회원가입 버튼 클릭시 수행
         btn_register.setOnClickListener(new View.OnClickListener() {
@@ -82,6 +83,46 @@ public class RegisterActivity extends AppCompatActivity {
 
             }
         });
+
+        btn_overlap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String userID = et_id.getText().toString();
+
+                Response.Listener<String> responseListener2 = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success) {
+                                // 회원가입 성공
+                                Toast.makeText(getApplicationContext(),"중복된 아이디입니다.",Toast.LENGTH_SHORT).show();
+                            }
+                            else {
+                                // 회원가입 실패
+                                Toast.makeText(getApplicationContext(),"사용할 수 있는아이디입니다.",Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                };
+
+                //서버로 요청
+                overlapRequest overlapRequest = new overlapRequest(userID,responseListener2);
+                RequestQueue queue2 = Volley.newRequestQueue(RegisterActivity.this);
+                queue2.add(overlapRequest);
+
+            }
+        });
+
+
 
 
 
